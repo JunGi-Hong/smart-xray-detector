ba64e185600922d034bf836fadf4a7b6347bc82a
Naddong <85006760+Naddong@users.noreply.github.com>
2026-02-14 23:57:13 +0900

    [BE] Feat: 사용자 로그인 기능 구현

--- SafeGate/BE/Spring/safe-guard/src/main/java/dna/safe_guard/service/SecurityConfig.java
+++ SafeGate/BE/Spring/safe-guard/src/main/java/dna/safe_guard/service/SecurityConfig.java
@@ -1,9 +1,15 @@
 package dna.safe_guard.service;
 
+import dna.safe_guard.repository.TokenBlacklistRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,5 +32,8 @@ public class SecurityConfig {
 
         return http.build();
     }
-    // ... PasswordEncoder 생략 ...
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
